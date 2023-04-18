1dc7e897e78384b8c31adc6c5dc89becc853cb6a
Juan Romero Peralta <juanmaromeroperalta9@gmail.com>
2023-04-18 14:48:11 -0500

    Correcion

--- js/codigo.js
+++ js/codigo.js
@@ -1,80 +1,103 @@
-let productos 
-let cantidadDeProductos;
-let seguirComprando
-const precioTotalProductos = (num1, num2) => num1 * num2
-const eleccionProductos = () => {
-    productos = parseInt(prompt(`Elige el producto que quieras llevar:\n1)Hamburguesa con queso 500$\n2)Hamburguesa doble con queso 650$\n3)Hamburguesa de pollo 450$\n4)Hamburgesa XXL 800$\n5)Hamburguesa Veguie 550$\n6)ESC`));
-    validacion()
+let productSelected
+let cartView = document.getElementById('cart')
+let products = [
+    {
+        id: 1,
+        nombre: "Hamburguesa con queso",
+        descripcion: "Hamburguesa con queso cheddar",
+        categoria: "Hamburguesa",
+        precio: 10.50,
+        stock: 5,
+        cantidad: 0
+    },
+    {
+        id: 2,
+        nombre: "Hamburguesa doble con queso",
+        categoria: "Hamburguesa",
+        descripcion: "Hamburguesa con 4 quesos cheddar",
+        precio: 15.00,
+        stock: 5,
+        cantidad: 0
+    },
+    {
+        id: 3,
+        nombre: "Hamburguesa de pollo",
+        categoria: "Hamburguesas",
+        descripcion: "Hamburguesa con pollo, lechuga y tomate",
+        precio: 7.99,
+        stock: 5,
+        cantidad: 0
+    },
+    {
+        id: 4,
+        nombre: "Hamburgesa XXL",
+        descripcion: "Hamburguesa con triple carne, panceta y queso cheddar",
+        categoria: "Hamburguesa",
+        precio: 22.75,
+        stock: 5,
+        cantidad: 0
+    },
+    {
+        id: 5,
+        nombre: "Hamburguesa Vegui",
+        categoria: "Hamburguesa",
+        descripcion: "Hamburguesa de lentejas con tomate, lechuga y condimentos",
+        precio: 16.99,
+        stock: 5,
+        cantidad: 0
+    }
+];
+const cart = [];
+const findProductById = (id) => {
+    let findProduct = products.find((product) => product.id === id)
+    return findProduct
 }
-const eleccionCantidad = () => {
-    cantidadDeProductos = parseInt(prompt("Cuantos productos elegidos va a llevar?"))
+const eleccionProductos = () => {
+    productSelected = parseInt(prompt(`Elige el producto que quieras llevar:\n1)Hamburguesa con queso\n2)Hamburguesa doble con queso\n3)Hamburguesa de pollo\n4)Hamburgesa XXL\n5)Hamburguesa Veguie\n6)ESC`));
+    validacion();
+}
+const eleccionCantidad = (product) => {
+    cantidadDeProductos = parseInt(prompt(`Cantidad de ${product.nombre} que vas a agregar al carrito?`));
+    return cantidadDeProductos
 }
 const seguirCompra = () => {
-    seguirComprando = parseInt(prompt(`Desea seguir Comprando?\n1:SI\n2: NO`))
-
+    let seguirComprando = parseInt(prompt(`Desea seguir Comprando?\n1:SI\n2: NO`))
+    return seguirComprando;
 }
 const validacion = () => {
-    if (isNaN(productos) || productos < 1 || productos > 6) {
+    if (isNaN(productSelected) || productSelected < 1 || productSelected > 6) {
         alert("La opcion elegida no es válida, por favor ingrese un numero del 1 al 6")
         eleccionProductos()
     }
     else {
-        while(productos !== 6)
-        {
-            let resultado
-            eleccionCantidad()
-            if (isNaN(cantidadDeProductos)) {
-                alert("La cantidad debe ser espresada en números")
+        let findProduct = findProductById(productSelected)
+        let catidadDeProducto = eleccionCantidad(findProduct)
+        if (isNaN(catidadDeProducto)) {
+            alert("La cantidad debe ser espresada en números")
+            eleccionCantidad(findProduct)
+        }
+        else {
+            if (cart.some(product => product.id === productSelected)) {
+                findProduct.cantidad += catidadDeProducto
             }
             else {
-                if (productos == 1) {
-                    resultado = precioTotalProductos(500, cantidadDeProductos)
-                    alert(`El total a abonar por ${cantidadDeProductos} hamburguesa con queso es $${resultado}`);
-                    seguirCompra()
-                    if(seguirComprando === 1 ){
-                        eleccionProductos()
-                    }
-                    break;
-                }
-                if (productos == 2) {
-                    resultado = precioTotalProductos(650, cantidadDeProductos)
-                    alert(`El total a abonar por ${cantidadDeProductos} hamburguesa doble con queso es $${resultado}`);
-                    seguirCompra();
-                    if(seguirComprando === 1 ){
-                        eleccionProductos();
-                    }
-                    break;
-                }
-                if (productos == 3) {
-                    resultado = precioTotalProductos(450, cantidadDeProductos)
-                    alert(`El total a abonar por ${cantidadDeProductos} hamburguesa de pollo es $${resultado}`);
-                    seguirCompra()
-                    if(seguirComprando === 1 ){
-                        eleccionProductos();
-                    }
-                    break;
-                }
-                if (productos == 4) {
-                    resultado = precioTotalProductos(800, cantidadDeProductos)
-                    alert(`El total a abonar por ${cantidadDeProductos} hamburguesa XXL es $${resultado}`);
-                    seguirCompra()
-                    if(seguirComprando === 1 ){
-                        eleccionProductos();
-                    }
-                    break;
-                }
-                if (productos == 5) {
-                    resultado = precioTotalProductos(800, cantidadDeProductos)
-                    alert(`El total a abonar por ${cantidadDeProductos} hamburguesa Veguie es $${resultado}`);
-                    seguirCompra()
-                    if(seguirComprando === 1 ){
-                        eleccionProductos();
-                    }
-                  break;
-                }
+                findProduct.cantidad += catidadDeProducto
+                cart.push(findProduct)
+            }
+            let continuarCompra = seguirCompra()
+            if (isNaN(continuarCompra)) {
+                alert("La opcion ingresada no existe")
+                seguirCompra()
+            }
+            if (continuarCompra === 1) {
+                eleccionProductos()
+            }
+            else {
+                let total = 0
+                cart.forEach(product => total += (product.precio * product.cantidad))
+                alert("El total de su carrito es de: $" + total)
             }
         }
     }
 }
 eleccionProductos()
-console.log("Gracias por usar nuestra aplicación")
